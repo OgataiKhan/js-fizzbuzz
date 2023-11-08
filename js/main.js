281e4c85d485074724b7b1cda569e5af39292d1e
OgataiKhan <badellino.kevin@gmail.com>
2023-11-08 15:25:20 +0100

    feat: apply different styles based on output

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const numbers = document.getElementById('numbers');
 
 for (let i = 1; i <= 100; i++) {
 
-    // Bonus 1: 8. Use createElement and append() to print our output into an html element
+    // Bonus 1: 8. Use createElement
     const numberBox = document.createElement('div');
     numberBox.classList.add('number-box');
 
@@ -17,10 +17,14 @@ for (let i = 1; i <= 100; i++) {
     let word = '';
 
     // 4. Use the condition "if (i % 3 === 0)" to append "Fizz" to the "word" variable for numbers divisible by 3  
-    if (i % 3 === 0) word += 'Fizz';
+    if (i % 3 === 0) {
+        word += 'Fizz';
+    }
 
     // 5. Use the condition "if (i % 5 === 0)" to append "Buzz" to the "word" variable for numbers divisible by 5  
-    if (i % 5 === 0) word += 'Buzz';
+    if (i % 5 === 0) {
+        word += 'Buzz';
+    }
     
     // 6. Use a condition to change the value of "output" to "word" if "word" is not empty (meaning it's "Fizz", "Buzz", or "FizzBuzz") 
     if (word !== '') {
@@ -30,6 +34,17 @@ for (let i = 1; i <= 100; i++) {
     // 7. Print the output to the console
     console.log(output);
 
+    // 9. Apply different styles based on whether the output is a number, "Fizz", "Buzz", or "FizzBuzz"
+
+    if (output === 'Fizz') {
+        numberBox.classList.add('fizz');
+    } else if (output === 'Buzz') {
+        numberBox.classList.add('buzz');
+    } else if (output === 'FizzBuzz') {
+        numberBox.classList.add('fizzbuzz');
+    }
+
+    // 8. Use append() to print our output into an html element
     numberBox.append(output);
     numbers.append (numberBox);
   }
